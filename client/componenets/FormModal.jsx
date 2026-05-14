b8da3bde7b3227fab4b9b71f9234afdeae537437
Ashley McKellar <ashley.l.mckellar@gmail.com>
2026-05-14 08:36:10 -0500

    updated button colors and modal sizing

--- client/componenets/FormModal.jsx
+++ client/componenets/FormModal.jsx
@@ -13,10 +13,11 @@ const FormModal = (props) => {
             contentLabel="Form Modal"
             className="custom-modal"
             overlayClassName="custom-overlay"
+             bodyOpenClassName="modal-open" 
         >
             <div>
                 <h2 className="modal-h2">Request Sent</h2>
-                <br></br>
+              
                 <p className="modal-p">
                     Your request was sent, and I&apos;ll get back to you as soon as
                     possible. Thanks!
