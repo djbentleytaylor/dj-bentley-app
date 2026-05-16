416e8905ea790dea4baf6235d8f1f66f1cfaf2aa
Ashley McKellar <ashley.l.mckellar@gmail.com>
2026-05-16 12:06:37 -0500

    updated routes

--- client/componenets/App.jsx
+++ client/componenets/App.jsx
@@ -3,26 +3,30 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './Navbar.jsx';
 import Gallery from './Gallery.jsx';
-import './style.css'
+import './style.css';
 import Footer from './Footer.jsx';
 import Main from './Main.jsx';
 
 function App() {
-
-  return (
-    <div>
-      <Routes>
-        <Route path="/" element={
-          <>
-            <Navbar />
-            <Main />
-          </>
-        } />
-        <Route path="/gallery" element={<Gallery />} />
-      </Routes>
-      <Footer />
-    </div>
-  )
+    return (
+    
+            <div>
+                <Routes>
+                    <Route
+                        path="/"
+                        element={
+                            <>
+                                <Navbar />
+                                <Main />
+                            </>
+                        }
+                    />
+                    <Route path="/gallery" element={<Gallery />} />
+                </Routes>
+                <Footer />
+            </div>
+     
+    );
 }
 
-export default App
+export default App;
